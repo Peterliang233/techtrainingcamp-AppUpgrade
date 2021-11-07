fd2462cf03b3b86450f0f3270389392a1284a5f4
Peterliang233 <ncuyanping666@126.com>
2021-11-07 22:29:22 +0800

    :sparkles: 添加数据库配置

--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -1,1 +1,41 @@
 package mysql
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Peterliang233/techtrainingcamp-AppUpgrade/config"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	Db *gorm.DB
+)
+
+// InitMysql 初始化数据库
+func InitMysql() {
+	var err error
+
+	Db, err = gorm.Open(config.DatabaseSetting.Db,
+		fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+			config.DatabaseSetting.DbUser,
+			config.DatabaseSetting.DbPassword,
+			config.DatabaseSetting.DbHost,
+			config.DatabaseSetting.DbPort,
+			config.DatabaseSetting.DbName,
+		))
+
+	if err != nil {
+		log.Println("数据库打开错误", err)
+	}
+
+	Db.SingularTable(true)
+
+	Db.DB().SetMaxIdleConns(10)
+
+	Db.DB().SetMaxOpenConns(100)
+
+	Db.DB().SetConnMaxLifetime(10 * time.Second)
+}
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import v1 "github.com/Peterliang233/techtrainingcamp-AppUpgrade/router/v1"
+import (
+	"github.com/Peterliang233/techtrainingcamp-AppUpgrade/database/mysql"
+	v1 "github.com/Peterliang233/techtrainingcamp-AppUpgrade/router/v1"
+)
 
 func main() {
 	router := v1.InitRouter()
+
+	mysql.InitMysql()
 
 	if err := router.Run(":9090"); err != nil {
 		panic(err)
