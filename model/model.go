0db245ba33aa3bc12f525ad736da16f30316c877
Peterliang233 <ncuyanping666@126.com>
2021-11-12 17:10:19 +0800

    :bug: 修改对象模型层的bug

--- model/model.go
+++ model/model.go
@@ -9,11 +9,11 @@ type User struct {
 // Rule 配置的新版本更新规则
 type Rule struct {
 	ID                   int    `json:"id"`
-	AID                  int    `json:"aid" label:"app的唯一标识" validate:"required"`
+	AppID                int    `json:"app_id" label:"app的唯一标识"`
 	Platform             string `json:"platform" label:"平台"`
 	DownloadURL          string `json:"download_url" label:"包的下载链接"`
 	UpdateVersionCode    string `json:"update_version_code" label:"当前包的版本号"`
-	Md5                  string `json:"md_5" label:"包的MD5"`
+	Md5                  string `json:"md5" label:"包的MD5"`
 	MaxUpdateVersionCode string `json:"max_update_version_code" label:"可升级的最大版本号"`
 	MinUpdateVersionCode string `json:"min_update_version_code" label:"可升级的最小版本号"`
 	MaxOSApi             int    `json:"max_os_api" label:"支持的最大操作系统版本"`
@@ -33,7 +33,7 @@ type Info struct {
 	ChannelNumber     string `json:"channel_number" label:"渠道，标识包的类型"`
 	VersionCode       string `json:"version_code" label:"应用的大版本"`
 	UpdateVersionCode string `json:"update_version_code" label:"应用的小版本"`
-	AID               int    `json:"aid" label:"app的唯一标识"`
+	AppID             int    `json:"app_id" label:"app的唯一标识"`
 	CPUArch           int    `json:"cpu_arch" label:"设备的CPU架构"`
 }
 
