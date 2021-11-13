7c483e152581be2ca4e592543275ba7f09c9c51d
Peterliang233 <ncuyanping666@126.com>
2021-11-13 23:28:01 +0800

    :zap: 优雅地关闭和重启

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/Peterliang233/techtrainingcamp-AppUpgrade/config"
 	"github.com/Peterliang233/techtrainingcamp-AppUpgrade/database/mysql"
 	"github.com/Peterliang233/techtrainingcamp-AppUpgrade/database/redis"
@@ -12,7 +20,28 @@ func main() {
 	mysql.InitMysql()
 	redis.InitRedis()
 
-	if err := router.Run(config.ServerSetting.HttpPort); err != nil {
-		panic(err)
+	server := &http.Server{
+		Addr:           config.ServerSetting.HttpPort,
+		Handler:        router,
+		MaxHeaderBytes: 1 << 20,
 	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("ShutDown Server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown, ", err)
+	}
+
+	log.Println("Server Exiting...")
 }
